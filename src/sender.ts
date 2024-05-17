9d01279afaefc5c746d6dfc929704fc9bbd42c88
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-05-17 12:21:40 -0300

    fix: class sender

--- src/sender.ts
+++ src/sender.ts
@@ -8,6 +8,11 @@ class Sender {
         this.initialize()
     }
 
+    async sendText(to: string, body: string) {
+        
+        this.client.sendText(to, body)
+    }
+
     private initialize() {
         const qrcode = (bae64Qrimg: string) => {}
 
@@ -15,6 +20,8 @@ class Sender {
 
         const start = (client: Whatsapp) => {
             this.client = client
+
+            this.sendText('5519900000000@c.us', 'Olá, tudo bem?')
         }
 
         create("zap-bot", qrcode, status)
